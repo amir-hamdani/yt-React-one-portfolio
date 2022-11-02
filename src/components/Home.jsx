e4f0537a2dc23fbb00377585d4d0c77a388e67eb
Syed Amir Shah Hamdani <hamdani1981@gmail.com>
2022-11-02 19:46:40 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">
                     I'm a Full Stack Developer
                 </h2>
-                <p className="text-gray-500 py-4 max-w-md">
+                <p className="text-gray-500 py-4 max-w-md"> 
                     I have 2 years of experience building and desiginig software.
                     Currently, I love to work on web application using technologies like React, tailwind, Next JS and GraphQL.
                 </p>
